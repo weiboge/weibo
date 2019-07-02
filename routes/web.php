9d4286e81d4f978562924a8a1adb09adfb42bb73
vagrant <vagrant@homestead>
2019-07-02 02:57:40 +0000

    查看用户列表

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Auth::routes();
 //Route::get('/home', 'HomeController@index')->name('home');
 //Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit','index']]);
