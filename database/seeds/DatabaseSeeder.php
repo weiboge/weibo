77de87b850e726af6435b9010c01d4b345119c1b
vagrant <vagrant@homestead>
2019-07-03 01:56:08 +0000

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersSeeder::class);
         $this->call(StatusesSeeder::class);
+        $this->call(FollowersSeeder::class);
     }
 }
